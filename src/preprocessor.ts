56fe5b030befbded9baab23f07ff73174f61f157
Andres Cera <Andres.cera@hotmail.com>
2026-01-03 00:18:36 -0500

    chore: update internal package references to new name

--- src/preprocessor.ts
+++ src/preprocessor.ts
@@ -2,7 +2,7 @@ import { persistPreprocessor } from "./plugins";
 
 export default function () {
 	console.warn(
-		"[WARN] The import of default of `@macfja/svelte-persistent-runes/preprocessor` is deprecated,\n       use `persistPreprocessor` of import `@macfja/svelte-persistent-runes/plugins`",
+		"[WARN] The import of default of `svelte-persistent-runes/preprocessor` is deprecated,\n       use `persistPreprocessor` of import `svelte-persistent-runes/plugins`",
 	);
 	return persistPreprocessor();
 }
